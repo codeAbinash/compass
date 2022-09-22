39317fc19edba922ee066a56ec4b723720029e4e
Abinash Karmakar <codeAbinash@gmail.com>
2022-09-22 17:36:42 +0530

    Added sw code

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ async function registerSW() {
     if ('serviceWorker' in navigator) {
         try {
             // Change the service worker URL to see what happens when the SW doesn't exist
-            const registration = await navigator.serviceWorker.register("../sw.js");
+            const registration = await navigator.serviceWorker.register("./sw.js");
             console.log('Service worker registered')
         } catch (error) {
             showResult("Error while registering: " + error.message);
